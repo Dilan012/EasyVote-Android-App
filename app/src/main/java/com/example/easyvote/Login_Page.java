2054385bc460bb0f3b97597c8f6d80324249f5c7
Dilan Madhusankha <dilanmadusanka5555@gmail.com>
2024-07-02 17:20:47 +0530

    initial commit

---
+++ app/src/main/java/com/example/easyvote/Login_Page.java
@@ -0,0 +1,95 @@
+package com.example.easyvote;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login_Page extends AppCompatActivity {
+
+    Button buttonlogin;
+    EditText username, password;
+    TextView create_account;
+    FirebaseAuth fAuth;
+
+    ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        create_account = findViewById(R.id.create_account);
+        username = findViewById(R.id.Username);
+        password = findViewById(R.id.Password);
+        fAuth = FirebaseAuth.getInstance();
+        buttonlogin = findViewById(R.id.login);
+
+        progressBar = findViewById(R.id.pbar2);
+        progressBar.setVisibility(View.INVISIBLE);
+
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login_Page.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+        buttonlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String Email = username.getText().toString();
+                String Password = password.getText().toString();
+
+                if(TextUtils.isEmpty(Email)){
+                    username.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(Password)){
+                    password.setError("Password is required");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                fAuth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login_Page.this,"Login is Successfull",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(Login_Page.this,HomePage.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else{
+                            Toast.makeText(Login_Page.this,"Wrong Email or password",Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.INVISIBLE);
+                            return;
+                        }
+                    }
+                });
+
+
+
+            }
+        });
+    }
+}
